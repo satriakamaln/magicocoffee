a69f0d0456fe9069107c9fee2642a71431965fa3
Satria Kamal Norsaid <satriakamaln@gmail.com>
2023-09-06 18:59:04 +0800

    searchbox

--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ document.querySelector("#hamburger-menu").onclick = () => {
 };
 
 //klik diluar sidebar -> tutup navbar
-const hamburger = document.querySelector("#hamburger-menu");
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove("active");
+    }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
     }
 });
 
@@ -17,7 +21,8 @@ document.addEventListener("click", function (e) {
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
 
-document.querySelector("#search-button").onclick = () => {
+document.querySelector("#search-button").onclick = (e) => {
     searchForm.classList.toggle("active");
     searchBox.focus();
+    e.preventDefault();
 };
