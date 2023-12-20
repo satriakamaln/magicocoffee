bd55d1362f080917a9b769aca2be7e4ddc616305
Satria Kamal Norsaid <satriakamaln@gmail.com>
2023-12-20 08:51:06 +0800

    shoppingcart and sc btn

--- js/script.js
+++ js/script.js
@@ -4,19 +4,6 @@ document.querySelector("#hamburger-menu").onclick = () => {
     navbarNav.classList.toggle("active");
 };
 
-//klik diluar sidebar -> tutup navbar
-const hm = document.querySelector("#hamburger-menu");
-const sb = document.querySelector("#search-button");
-
-document.addEventListener("click", function (e) {
-    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
-        navbarNav.classList.remove("active");
-    }
-    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
-        searchForm.classList.remove("active");
-    }
-});
-
 // class toggle class active search form
 const searchForm = document.querySelector(".search-form");
 const searchBox = document.querySelector("#search-box");
@@ -26,3 +13,28 @@ document.querySelector("#search-button").onclick = (e) => {
     searchBox.focus();
     e.preventDefault();
 };
+
+//class toggle class active form
+const shoppingCart = document.querySelector(".shopping-cart");
+
+document.querySelector("#shopping-cart-button").onclick = (e) => {
+    shoppingCart.classList.toggle("active");
+    e.preventDefault();
+};
+
+//klik diluar sidebar -> tutup navbar
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+const scb = document.querySelector("#shopping-cart-button");
+
+document.addEventListener("click", function (e) {
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+    if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove("active");
+    }
+    if (!scb.contains(e.target) && !shoppingCart.contains(e.target)) {
+        shoppingCart.classList.remove("active");
+    }
+});
